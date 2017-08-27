6a4496b657a0ea338a89bd69f3fffbb15315f455
Chandler <chndlr.wllm@gmail.com>
2017-08-26 23:58:11 -0500

    updated html, css, js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,64 +6,103 @@
 
 // $("#start-button").click(alert("hey there"));
 
-
+$("#start-game").on("click", run);
 
 // GLOBAL VARIABLES
 
-var correctAnswers = "";
-var incorrectAnswers = "";
-var unanswered = "";
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
 
-var countdown = 5;
+var countdown = 10;
 var intervalId;
 
-// var qAndA = {
-// 	s1: []
-// 	q1: "What color is the sky",
-// 	a1a: "Blue",
-// 	a1b: "Red"
-// }
+var qA = {
 
-var questions = {
-	q1: "What color is the sky",
-	q2: "What is the capital of Sweden"
+	questions: 
+	[
+		"In what month is the Earth closest to the sun?", 
+		"What is the capital of Sweden?"
+	],
+	answers: 
+	[
+		["February", "January", "September", "December"], 
+		["Norway", "Scandinavia", "Austin", "Stockholm"]
+	]
+
 };
 
-var answers = {
-	a1: ["blue", "green", "red", "black"],
-	a2: ["Norway", "Scandinavia", "Austin", "Stockholm"]
-};
-
-$("#start-game").on("click", run);
+var arrCorrect = [qA.answers[0][1], qA.answers[1][3]];
+// console.log(arrCorrect);
 
 
 // FUNCTIONS 
 
 function run() {
 	intervalId = setInterval(decrement, 1000);
-	// $("#question").html("<h2>" + qAndA.q1 + "</h2>");
-	// $("#answers").html("<h2>" + qAndA.a1a + "</h2>");
-	$("#questions").html("<h2>" + questions.q1 + "</h2>");
-	$("#answers").html("<h2>" + answers.a1 + "</h2>");
+	$("button").remove();
+	launchQA();
 }
 
 function decrement() {
 	countdown--;
 	$("#countdown").html("<h2>Time remaining: " + countdown + "</h2>");
 		if (countdown === 0) {
-	    	stop();
-	    	alert("Time Up!");
-		}
+	    stop();
+	    console.log("Time Up!");
+	}
 }
 
 function stop() {
 	clearInterval(intervalId);
 }
 
+function reset() {
+	$("button").show();
+}
+
+function launchQA() {
+	
+	for (var i = 0; i < qA.questions.length; i++) {
+	
+	$("#questions").html("<h2>" + qA.questions[i] + "</h2>");
+	$("#answer0").html("<h3>" + qA.answers[i][0] + "</h3>");
+	$("#answer1").html("<h3>" + qA.answers[i][1] + "</h3>");
+	$("#answer2").html("<h3>" + qA.answers[i][2] + "</h3>");
+	$("#answer3").html("<h3>" + qA.answers[i][3] + "</h3>");
+	};
+
+
+
+	jQuery(".answer").click(function() {
+    var userChoice = jQuery(this).attr("id");
+    console.log(userChoice);
+    return userChoice;
+	});
+
+	// console.log(userChoice);
+
+
+	// $('.answer').click(function(){
+	//     console.log(this.id);
+	// });
+	
+
+	// if (userChoice === arrCorrect[i]) {
+	// 	alert("Correct!");
+	// }
+
+}
+
+
+
+
+
 
 // MAIN PROCESS
 
-// $("#start-button").on("click", start);
+
+
 
 
 
