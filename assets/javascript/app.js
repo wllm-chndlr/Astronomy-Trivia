ded2b08de011ba9047bc6471adfa7b2c6f924d3e
Chandler <chndlr.wllm@gmail.com>
2017-08-29 07:02:22 -0500

    q&A scrolling and displaying, result screen displaying

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,8 @@ var countdown = 11;
 var intervalId;
 
 var blazingIndex = 0;
-var correctAnswerIndex = 0;
+// var correctAnswerIndex = 0;
+var correctAnswerIndex;
 
 var qaArray = [
 	{
@@ -28,8 +29,6 @@ var qaArray = [
 		question: "In what month is the Earth closest to the sun?",
 		answers: ["February", "January", "September", "December"],
 		correctAnswerIndex: 1
-		// pic_correct: 'PATH',
-		// pic_wrong: 'PATH',
 	},
 	{
 		question: "Which planet's axial rotation is opposite to the other planets in the solar system?",
@@ -40,8 +39,11 @@ var qaArray = [
 		question: "The Sun's core releases the the equivalent of _______ nuclear bombs every second.",
 		answers: ["100,000", "100 million", "100 billion", "100 trillion"],
 		correctAnswerIndex: 2
-		// pic_correct: 'PATH',
-		// pic_wrong: 'PATH',
+	},
+	{
+		question: "We can only detect about _______ of the matter in the universe.",
+		answers: ["5%", "10%", "15%", "20%"],
+		correctAnswerIndex: 0
 	}
 ];
 
@@ -67,7 +69,7 @@ function stop() {
 
 function launchQA() {
 
-	$("button").remove();
+	$("button").hide();
 	countdown = 11;
 	startCountdown();
 
@@ -96,42 +98,52 @@ function userGuess() {
 }
 
 function correctAnswer() {
-	$('#main-panel').empty().html("<h3>Correct!</h3>");
+	// $('#main-panel').empty().html("<h3>Correct!</h3>");
+	$('#results').html("<h3>Correct!</h3>");
 	correctAnswers++;
 	console.log("Correct answers: " + correctAnswers);
 	setTimeout(advance, 3000);
 }
 
 function incorrectAnswer() {
-	$('#main-panel').empty().html("<h3>Stupid! You're so stupid! <br><br> The correct answer was XXXXX.</h3>");
+	// $('#main-panel').empty().html("<h3>Stupid! You're so stupid! <br><br> The correct answer was XXXXX.</h3>");
+	$('#results').html("<h3>Wrong!</h3>");
 	incorrectAnswers++;
 	console.log("Incorrect answers: " + incorrectAnswers);
 	setTimeout(advance, 3000);
 }
 
 function timeUp() {
-	$('#main-panel').empty().html("<h3>Time's up! <br><br> The correct answer was XXXXX.</h3>");
+	// $('#main-panel').empty().html("<h3>Time's up! <br><br> The correct answer was XXXXX.</h3>");
+	$('#results').html("<h3>Time's up!</h3>");
 	unanswered++;
 	console.log("Unanswered: " + unanswered);
 	setTimeout(advance, 3000);
 }
 
 function advance() {
-	$('#main-panel').empty();
+	$('#results').empty();
 	blazingIndex++;
-	launchQA();
-	console.log(qaArray[blazingIndex].answers[1]);
+
+	if (blazingIndex < qaArray.length) {
+		launchQA();
+	}
+	else {
+		resultScreen();
+	}
+	
 }
 
 function resultScreen() {
 
-	if (blazingIndex>10) {
-	$("#questions").html("All done!");
-	$('#answer0').html("Correct answers: " + correctAnswers);
-	$('#answer1').html("Incorrect answers: " + incorrectAnswers);
-	$('#answer2').html("Unanswered: " + unanswered);
-	$('#results').html("Start over?"); // need to add reset
-	};
+	stop();
+
+	$("#questions").html("<h3>All done!</h3");
+	$('#answer0').html("<h3>Correct answers: " + correctAnswers + "</h3>");
+	$('#answer1').html("<h3>Incorrect answers: " + incorrectAnswers + "</h3");
+	$('#answer2').html("<h3>Unanswered: " + unanswered + "</h3>");
+	$('#answer3').hide();
+	$('#results').html("<h3>Start over?</h3>"); // need to add reset
 
 }
 
