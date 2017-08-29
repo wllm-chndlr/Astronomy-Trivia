93f07a0a63f9b7213e5c27880cba5cd21fad9629
Chandler <chndlr.wllm@gmail.com>
2017-08-28 19:05:05 -0500

    updated reset, launch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,9 +9,11 @@ $("#start-game").on("click", launchQA);
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
-var i = 0;
+var currentIndex = -1;
 var countdown = 10;
 var intervalId;
+var timeBetweenQuestions;
+var correctAnswerIndex;
 
 var qaArray = [
 	{
@@ -30,6 +32,7 @@ var qaArray = [
 
 	}
 	];
+
 
 // FUNCTIONS 
 
@@ -56,19 +59,22 @@ function launchQA() {
 	$("button").remove();
 	startCountdown();
 
-	for (var i = 0; i < qaArray[i].length; i++) {
-	};
+	for (var currentIndex = 0; currentIndex < qaArray.length; currentIndex++) {
+	
+	$("#questions").html("<h2>" + qaArray[currentIndex].question + "</h2>");
+	$("#answer0").html("<h3>" + qaArray[currentIndex].answers[0] + "</h3>");
+	$("#answer1").html("<h3>" + qaArray[currentIndex].answers[1] + "</h3>");
+	$("#answer2").html("<h3>" + qaArray[currentIndex].answers[2] + "</h3>");
+	$("#answer3").html("<h3>" + qaArray[currentIndex].answers[3] + "</h3>");
 
-	$("#questions").html("<h2>" + qaArray[i].question + "</h2>");
-	$("#answer0").html("<h3>" + qaArray[i].answers[0] + "</h3>");
-	$("#answer1").html("<h3>" + qaArray[i].answers[1] + "</h3>");
-	$("#answer2").html("<h3>" + qaArray[i].answers[2] + "</h3>");
-	$("#answer3").html("<h3>" + qaArray[i].answers[3] + "</h3>");
+	console.log(qaArray[currentIndex].question);
+
+	};
 
   $('.answer').click(function() {
   	stop();
   	var userAnswer = $('.answer').index(this);
-	  	if (userAnswer === qaArray[i].correctAnswerIndex) {
+	  	if (userAnswer === qaArray[currentIndex].correctAnswerIndex) {
 				correctAnswer();
 				// advance();
 	  	}
@@ -84,34 +90,50 @@ function correctAnswer() {
 	$('#main-panel').empty().html("<h3>Correct!</h3>");
 	correctAnswers++;
 	console.log("Correct answers: " + correctAnswers);
-	advance();
+	timeBetweenQuestions = setTimeout(function() {
+    console.log("next question");
+    advance();
+  }, 3000);
 }
 
 function incorrectAnswer() {
-	$('#main-panel').empty().html("<h3>Stupid! You're so stupid! <br><br> The correct answer was " + qaArray[i].answers[correctAnswerIndex] + ".</h3>");
+	$('#main-panel').empty().html("<h3>Stupid! You're so stupid! <br><br> The correct answer was XXXXX.</h3>");
 	incorrectAnswers++;
 	console.log("Incorrect answers: " + incorrectAnswers);
-	console.log(qaArray[i].answers[correctAnswerIndex]);
-	advance();
+	// console.log(qaArray[i].answers[correctAnswerIndex]);
+	timeBetweenQuestions = setTimeout(function() {
+    console.log("next question");
+    advance();
+  }, 3000);
 }
 
 function timeUp() {
 	$('#main-panel').empty().html("<h3>Time's up! <br><br> The correct answer was XXXXX.</h3>");
 	unanswered++;
 	console.log("Unanswered: " + unanswered);
-	advance();
+	timeBetweenQuestions = setTimeout(function() {
+    console.log("next question");
+    advance();
+  }, 3000);
 }
 
 function advance() {
-	i++;
+	currentIndex++;
+	countdown = 10;
+	startCountdown();
+
 }
 
 function resultScreen() {
+
+	if (currentIndex>10) {
 	$("#questions").html("All done!");
 	$('#answer0').html("Correct answers: " + correctAnswers);
 	$('#answer1').html("Incorrect answers: " + incorrectAnswers);
 	$('#answer2').html("Unanswered: " + unanswered);
 	$('#results').html("Start over?"); // need to add reset
+	};
+
 }
 
 function reset() {
